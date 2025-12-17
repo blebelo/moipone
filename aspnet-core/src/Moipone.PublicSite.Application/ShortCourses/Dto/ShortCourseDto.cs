97fc56d0e603c33779aacac1f45b9e7b0b21ac8a
=Benny= <blebelo023@student.wethinkcode.co.za>
2025-12-18 00:16:02 +0200

    Implemented DTO & Inteface

---
+++ aspnet-core/src/Moipone.PublicSite.Application/ShortCourses/Dto/ShortCourseDto.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Moipone.PublicSite.Domain.ShortCourses;
+using Moipone.PublicSite.Students.Dto;
+using System;
+using System.Collections.Generic;
+
+namespace Moipone.PublicSite.ShortCourses.Dto
+{
+    [AutoMap(typeof(ShortCourse))]
+    public class ShortCourseDto : EntityDto<Guid>
+    {
+        #region Core Details
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int? Capacity { get; set; }
+        public string? Requirements { get; set; }
+        public string? Code { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? StartDate { get; set; }
+        public TimeSpan? Duration { get; set; }
+        #endregion
+
+        #region Enrollment Data
+        public ICollection<StudentDto> EnrolledStudents { get; set; }
+        //public ICollection<CourseApplicationDto> Applications { get; set; }
+        #endregion
+    }
+}
---
+++ aspnet-core/src/Moipone.PublicSite.Application/ShortCourses/IShortCourseAppService.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services;
+using Moipone.PublicSite.ShortCourses.Dto;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Moipone.PublicSite.ShortCourses
+{
+    public interface IShortCourseAppService : IAsyncCrudAppService<ShortCourseDto, Guid>
+    {
+        Task<ShortCourseDto> GetByCodeAsync(string code);
+        Task<ShortCourseDto> OpenApplicationsAsync(Guid id);
+        Task<ShortCourseDto> CloseApplicationsAsync(Guid id);
+        Task<ShortCourseDto> ReopenApplicationsAsync(Guid id);
+        Task<List<ShortCourseDto>> GetOpenCoursesAsync();
+    }
+}
