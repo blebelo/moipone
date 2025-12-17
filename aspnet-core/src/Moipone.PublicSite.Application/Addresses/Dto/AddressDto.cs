89b90f62e203e9cd44ac1c93503d98734ef852e0
=Benny= <blebelo023@student.wethinkcode.co.za>
2025-12-17 22:03:42 +0200

    Refactored AddresDto

--- aspnet-core/src/Moipone.PublicSite.Application/Addresses/Dto/AddressDto.cs
+++ aspnet-core/src/Moipone.PublicSite.Application/Addresses/Dto/AddressDto.cs
@@ -8,6 +8,7 @@ namespace Moipone.PublicSite.Addresses.Dto
     [AutoMap(typeof(Address))]
     public class AddressDto : EntityDto<Guid>
     {
+        public string Street { get; set; }
         public string Suburb { get; set; }
         public string City { get; set; }
         public string PostalCode { get; set; }
